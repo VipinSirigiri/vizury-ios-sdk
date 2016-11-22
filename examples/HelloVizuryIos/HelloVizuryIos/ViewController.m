5417e76cbcaf97aa27b63ea5b8da0b2446062e84
Anurag Bhoumick <anurag.bhowmick@vizury.com>
2016-11-22 13:01:06 +0530

    adding examples for sending json data as attribute

--- examples/HelloVizuryIos/HelloVizuryIos/ViewController.m
+++ examples/HelloVizuryIos/HelloVizuryIos/ViewController.m
@@ -35,4 +35,49 @@
     
     [VizuryEventLogger logEvent:@"productPage" WithAttributes:attributeDictionary];
 }
+
+// you can send the attributes as JSONArray or JSONObject also
+- (void)sendCustomEvent{
+
+    // sending a JSONObject
+    NSDictionary *productDetail = [NSDictionary dictionaryWithObjectsAndKeys:
+                                   @"62112",@"product_id",
+                                   @"1",@"quantity",
+                                   @"50", @"price",
+                                   nil];
+
+    NSData *productjsonData = [NSJSONSerialization dataWithJSONObject:productDetail options:0 error:nil];
+    NSString *productjsonStr = [[NSString alloc] initWithData:productjsonData encoding:NSUTF8StringEncoding];
+
+    NSDictionary *productjsonDictionary = [NSDictionary dictionaryWithObjectsAndKeys:
+                                           productjsonStr,@"viz_data",
+                                           nil];
+    [VizuryEventLogger logEvent:@"productDetails" WithAttributes:productjsonDictionary];
+
+    // sending a JSON Array of Objects
+    NSMutableArray *products = [[NSMutableArray alloc] init];
+    NSDictionary *productDetail1 = [NSDictionary dictionaryWithObjectsAndKeys:
+                                    @"62112",@"product_id",
+                                    @"1",@"quantity",
+                                    @"50", @"price",
+                                    nil];
+
+    NSDictionary *productDetail2 = [NSDictionary dictionaryWithObjectsAndKeys:
+                                    @"123123",@"product_id",
+                                    @"1",@"quantity",
+                                    @"50", @"price",
+                                    nil];
+
+    [products addObject:productDetail1];
+    [products addObject:productDetail2];
+
+    NSData *wishListjsonData = [NSJSONSerialization dataWithJSONObject:products options:0 error:nil];
+    NSString *wishListjsonStr = [[NSString alloc] initWithData:wishListjsonData encoding:NSUTF8StringEncoding];
+
+    NSDictionary *wishListjsonDictionary = [NSDictionary dictionaryWithObjectsAndKeys:
+                                    wishListjsonStr,@"viz_data",
+                                    nil];
+
+    [VizuryEventLogger logEvent:@"wishlist" WithAttributes:wishListjsonDictionary];
+}
 @end
